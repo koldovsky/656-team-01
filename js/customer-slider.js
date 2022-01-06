13e0d0e2b2121e190da94905204057c16823c1f7
Volodymyr Petlyra <vovapetlyra@gmail.com>
2022-01-06 13:18:33 +0200

    add colorfor button

--- js/customer-slider.js
+++ js/customer-slider.js
@@ -1,28 +1,28 @@
 (function() {
     const slides = [
       `<article class="slider__item">
-      <img width="120" src="img/photo_amanda.webp" alt="photo_amanda">
-      <h2>Amanda Peterson</h2>
-      <p>“ZEN Cottages created a perfect vacation house for us! It's a place comfortable and spacious enough to
+      <img id="customer__photo" src="img/photo_amanda.webp" alt="photo_amanda">
+      <h2 id="customer__name">Amanda Peterson</h2>
+      <p id="customer-reviews__text">“ZEN Cottages created a perfect vacation house for us! It's a place comfortable and spacious enough to
         invite
         friends over. They deserve the highest grade!”
       </p>
-      <h3>11 MARCH, 2021</h3>
+      <h3 id="reviews__date">11 MARCH, 2021</h3>
     </article>`,
       `<article class="slider__item">
-      <img width="120" src="img/photo_jeff.webp" alt="photo_jeff">
-      <h2>Jeff Gordons</h2>
-      <p>“We bought the Shast model because we wanted something simple yet proven good. It was one of the best
+      <img id="customer__photo" src="img/photo_jeff.webp" alt="photo_jeff">
+      <h2 id="customer__name">Jeff Gordons</h2>
+      <p id="customer-reviews__text">“We bought the Shast model because we wanted something simple yet proven good. It was one of the best
         decisions for my family! Thanks for our new favorite place to stay.”
       </p>
-      <h3>13 JUNE, 2021</h3>
+      <h3 id="reviews__date">13 JUNE, 2021</h3>
     </article>`,
     `<article class="slider__item">
-    <img width="120" src="img/photo_elizabeth.webp" alt="photo_elizabeth">
-    <h2>Elizabeth Smith</h2>
-    <p>“My husband and I travel a lot, so we wanted something to make our travels more comfortable. As it turned
+    <img id="customer__photo" src="img/photo_elizabeth.webp" alt="photo_elizabeth">
+    <h2 id="customer__name">Elizabeth Smith</h2>
+    <p id="customer-reviews__text">“My husband and I travel a lot, so we wanted something to make our travels more comfortable. As it turned
       out, ZEN Cottages had the right solution for us.”</p>
-    <h3>21 OCTOBER, 2020</h3>
+    <h3 id="reviews__date">21 OCTOBER, 2020</h3>
     </article>`,
       ];
     let currentSlide = 0;
